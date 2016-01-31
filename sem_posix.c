5e2dc2a1b5a81b8c34fadbb57947f0e5a289562f
root <root@localhost.localdomain>
2016-01-31 16:05:58 +0300

    1

---
+++ sem_posix.c
@@ -0,0 +1,8 @@
+#include <fcntl.h>           /* For O_* constants */
+#include <sys/stat.h>        /* For mode constants */
+#include <semaphore.h>
+
+int main(int argc, char **argv) {
+    sem_t *mq = sem_open("/test.sem", O_CREAT, 0666, 66);
+    perror("Errno after sem_open: ");
+}
--- sem_sysv.c
+++ sem_sysv.c
@@ -3,20 +3,20 @@
 #include <unistd.h>
 
 int main(int argc, char** argv) {
-	key_t key = ftok("/tmp/sem.temp", 0);
-	int sems = semget(key, 16, 0666 | IPC_CREAT);
-	perror("Errno after semcreation: ");
+    key_t key = ftok("/tmp/sem.temp", 0);
+    int sems = semget(key, 16, IPC_CREAT);
+    perror("Errno after semcreation: ");
     
     struct sembuf sops[16];
-	for (int i = 0; i < 16; i++) {
-	    sops[i].sem_num = i;
-	    sops[i].sem_op = i;
-	    sops[i].sem_flg = 0;
-	}
-	semop(sems, sops, 16);
-	perror("Errno after semop: ");
+    for (int i = 0; i < 16; i++) {
+        sops[i].sem_num = i;
+        sops[i].sem_op = i;
+        sops[i].sem_flg = 0;
+    }
+    semop(sems, sops, 16);
+    perror("Errno after semop: ");
 	
-	pause();
+    pause();
 	
-	return 0;
+    return 0;
 }
