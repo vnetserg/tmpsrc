a7ee7d7f0f6f86084930f363b6c1669755a15a66
root <root@localhost.localdomain>
2016-01-31 15:40:21 +0300

    1

--- sem_sysv.c
+++ sem_sysv.c
@@ -5,10 +5,12 @@
 int main(int argc, char** argv) {
 	key_t key = ftok("/tmp/sem.temp", 0);
 	int sems = semget(key, 16, IPC_CREAT);
+	perror("Errno after semcreation: ");
 	for (int i = 0; i < 16; i++) {
 	    unsigned short semval = i;
 	    semctl(sems, i, SETVAL, semval);
 	}
+	perror("Errno after semctl: ");
 	pause();
 	return 0;
 }
