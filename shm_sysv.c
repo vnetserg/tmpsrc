393f6f153699b568d4fdda5bbd68e8c257f54aa0
root <root@localhost.localdomain>
2016-01-31 16:30:47 +0300

    123

---
+++ shm_sysv.c
@@ -0,0 +1,16 @@
+#include <sys/shm.h>
+
+int main(int argc, char** argv) {
+    key_t key = ftok("/tmp/mem.temp", 0);
+    int shm = shmget(key, 1048576, IPC_CREAT);
+    perror("Errno after shmget: ");
+    
+    char* data = shmat(shm, (void*)0, 0);
+    perror("Errno after shmgat: ");
+    
+    for (int i = 0; i < 1048576; i++)
+        data[i] = (char)42;
+    perror("Errno after writing: ");
+    
+    return 0;
+}
