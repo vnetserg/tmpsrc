86aeae3467a2664db7eb79194222481f7c57bf5d
root <root@localhost.localdomain>
2016-01-31 15:52:49 +0300

    q

--- sem_sysv.c
+++ sem_sysv.c
@@ -4,13 +4,19 @@
 
 int main(int argc, char** argv) {
 	key_t key = ftok("/tmp/sem.temp", 0);
-	int sems = semget(key, 16, IPC_CREAT);
+	int sems = semget(key, 16, 0666 | IPC_CREAT);
 	perror("Errno after semcreation: ");
+    
+    struct sembuf sops[16];
 	for (int i = 0; i < 16; i++) {
-	    unsigned short semval = i;
-	    semctl(sems, i, SETVAL, semval);
+	    sops[i].sem_num = i;
+	    sops[i].sem_op = i;
+	    sops[i].sem_flg = 0;
 	}
-	perror("Errno after semctl: ");
+	semop(sems, sops, 16);
+	perror("Errno after semop: ");
+	
 	pause();
+	
 	return 0;
 }
